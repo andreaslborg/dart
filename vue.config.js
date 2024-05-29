2468431069a4a233a03784ff8f23e1fa0790d72a
Andreas Løvig Borg <72602768+andreaslborg@users.noreply.github.com>
2024-05-29 20:56:51 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/dart/'
-    : '/'
+    ? '/dart/'  
+    : '/',
+  devServer: {
+    historyApiFallback: true
+  }
 })
